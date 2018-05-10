011a44ce79810abd55c6be09eec28014fade310f
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-10 22:46:21 +0800

    m

--- script/raft.sh
+++ script/raft.sh
@@ -9,6 +9,6 @@ done
 
 for v in `seq 1 $NUM`
 do
-	RUN="cd home/node$v && chmod 755 ./raft-init.sh && ./raft-start.sh"
+	RUN="cd home/node$v && chmod 755 ./raft-start.sh && ./raft-start.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN" &
 done
