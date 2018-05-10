ce71c6be0594531639631c3734eb6f97a78284ec
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-10 16:13:34 +0800

    deploy

--- script/raft.sh
+++ script/raft.sh
@@ -2,14 +2,13 @@ NUM=$1
 echo "init all node"
 for v in `seq 1 $NUM`
 do
-	RUN="chmod 777 ./raft-init.sh"
-	RUN="cd home/node$v && sh stop.sh && ./raft-init.sh"
+	
+	RUN="cd home/node$v && sh stop.sh &&  chmod 755 ./raft-init.sh && ./raft-init.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN"
 done
 
 for v in `seq 1 $NUM`
 do
-	RUN="chmod 777 ./raft-start.sh"
-	RUN="cd home/node$v && ./raft-start.sh"
+	RUN="cd home/node$v && chmod 755 ./raft-init.sh && ./raft-start.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN" &
 done
